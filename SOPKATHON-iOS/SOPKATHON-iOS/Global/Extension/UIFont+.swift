b450f474e408c5d7838bdc56b48c4c96ac3faf16
cchanmi <gkfn980@hs.ac.kr>
2022-11-20 01:37:38 +0900

    [Feat] 폰트 설정 함수 추가

--- SOPKATHON-iOS/SOPKATHON-iOS/Global/Extension/UIFont+.swift
+++ SOPKATHON-iOS/SOPKATHON-iOS/Global/Extension/UIFont+.swift
@@ -5,4 +5,14 @@
 //  Created by 황찬미 on 2022/11/20.
 //
 
-import Foundation
+import UIKit
+
+enum FontName: String {
+    case hi
+}
+
+extension UIFont {
+    static func font(_ style: FontName, ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: style.rawValue, size: size)!
+    }
+}
