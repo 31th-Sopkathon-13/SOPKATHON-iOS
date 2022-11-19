d1817dafe03de30e1fd4a47b4acc266f45bbe1f5
cchanmi <gkfn980@hs.ac.kr>
2022-11-20 01:46:46 +0900

    [Add] - Color 추가

---
+++ SOPKATHON-iOS/SOPKATHON-iOS/Global/Extension/UIColor+.swift
@@ -0,0 +1,35 @@
+//
+//  UIColor+.swift
+//  SOPKATHON-iOS
+//
+//  Created by 황찬미 on 2022/11/20.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var black: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray1: UIColor {
+        return UIColor(red: 44.0 / 255.0, green: 44.0 / 255.0, blue: 46.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray2: UIColor {
+        return UIColor(red: 142.0 / 255.0, green: 142.0 / 255.0, blue: 147.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray3: UIColor {
+        return UIColor(red: 209.0 / 255.0, green: 209.0 / 255.0, blue: 214.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var primary: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 90.0 / 255.0, blue: 90.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var white: UIColor {
+        return UIColor(red: 0.0 / 255.0, green: 0.0 / 255.0, blue: 0.0 / 255.0, alpha: 1.0)
+    }
+}
