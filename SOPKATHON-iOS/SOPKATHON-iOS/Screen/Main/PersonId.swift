c8d39459c4cb612e58685ca5d96da57b440bd3b5
jeongdung-eo <jeuniis2@gmail.com>
2022-11-20 02:32:22 +0900

    [Feat] #5 - Main CollectionView Cell 더미데이터

--- SOPKATHON-iOS/SOPKATHON-iOS/Screen/Main/PersonId.swift
+++ SOPKATHON-iOS/SOPKATHON-iOS/Screen/Main/PersonId.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct PersonId1 : Hashable{
+    var name : String
+}
+extension PersonId1 {
+    static var idList : [PersonId1] = [PersonId1(name: "김정은"),
+                                       PersonId1(name: "황찬미")
+    ]
+}
+struct PersonId2 : Hashable{
+    var name : String
+}
+extension PersonId2 {
+    static var idList : [PersonId2] = [PersonId2(name: "정윤선"),
+                                       PersonId2(name: "김민서")
+    ]
+}
+struct PersonId3 : Hashable{
+    var name : String
+}
+extension PersonId3 {
+    static var idList : [PersonId3] = [PersonId3(name: "김채현"),
+                                       PersonId3(name: "이성현")
+    ]
+}
+
+
